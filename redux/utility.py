1c668c93a7be89ebba3d27635126b1dc0a2eb278
pulokas <pulokas>
2011-12-05 20:29:54 +0000

    fixed conversions for demjson module

--- redux/utility.py
+++ redux/utility.py
@@ -36,25 +36,39 @@ def kwargs_to_request(**kwargs):
 	request = '&'.join(args)
 	return request
 
+## Extra things we would like to convert to JSON compatible
+def json_convert(obj):
+		## convert numpy types to built-in python types
+		if isinstance(obj, numpy.bool_):
+			return bool(obj)
+		elif isinstance(obj, numpy.integer):
+			return long(obj)
+		elif isinstance(obj, numpy.floating):
+			return float(obj)
+		elif isinstance(obj, numpy.complexfloating):
+			return complex(obj)
+		elif isinstance(obj, numpy.dtype):
+			return str(obj)
+		elif isinstance(obj, numpy.ndarray):
+			return obj.tolist()	
+		raise ValueError('cannot convert %s' % (obj,))
+
+## now use the conversion function in either of the two modules
 if jsonmod == 'demjson':
-	json_encode = demjson.encode
+	class ReduxJSON(demjson.JSON):
+		def encode_default(self, obj, nest_level=0):
+			newobj = json_convert(obj)
+			return self.encode(newobj, nest_level)
+	json_inst = ReduxJSON()
+	json_encode = json_inst.encode
+
 elif jsonmod == 'json':
 	class ReduxJSONEncoder(json.JSONEncoder):
 		def default(self, obj):
-			## convert numpy types to built-in python types
-			if isinstance(obj, numpy.bool_):
-				return bool(obj)
-			elif isinstance(obj, numpy.integer):
-				return long(obj)
-			elif isinstance(obj, numpy.floating):
-				return float(obj)
-			elif isinstance(obj, numpy.complexfloating):
-				return complex(obj)
-			elif isinstance(obj, numpy.dtype):
-				return str(obj)
-			elif isinstance(obj, numpy.ndarray):
-				return obj.tolist()	
-			return json.JSONEncoder.default(self, obj)
+			try:
+				return json_convert(obj)
+			except ValueError:
+				return json.JSONEncoder.default(self, obj)
 	
 	def json_encode(input):
 			outstring = json.dumps(input, cls=ReduxJSONEncoder)
