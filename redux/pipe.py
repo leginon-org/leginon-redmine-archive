89bf937e827063be9fcf2ecc5133b0b29d34193c
pulokas <pulokas>
2012-01-12 23:10:02 +0000

    allow a decimal point in a shape value

--- redux/pipe.py
+++ redux/pipe.py
@@ -43,7 +43,7 @@ def shape_converter(value):
 	'''size must be a shape of the form "AxB"'''
 	# first convert value to sequence of numbers
 	if isinstance(value, types.StringTypes):
-		numbers = re.findall('\d+', value)
+		numbers = re.findall('[\d.]+', value)
 	else:
 		numbers = list(value)
 	# now convert numbers to integers
