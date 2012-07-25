bebc8fcc726c5fd99db29aa2556eaf094d7c8e78
pulokas <pulokas>
2012-07-25 19:01:08 +0000

    refs #1762, cleaned up and moved some functions to Constrainer class

--- redux/cachefs.py
+++ redux/cachefs.py
@@ -15,11 +15,10 @@ class FileObjectWrapper(object):
 	'''
 	Override the close method, so we can track changes to files.
 	'''
-	def __init__(self, fileobj, closecallback, name, mode):
+	def __init__(self, fileobj, closecallback, name):
 		self.fileobj = fileobj
 		self.closecallback = closecallback
 		self.name = name
-		self.mode = mode
 
 	def __getattr__(self, attr):
 		if attr in self.__dict__:
@@ -29,12 +28,63 @@ class FileObjectWrapper(object):
 	def close(self):
 		# close the file, then run close callback
 		self.fileobj.close()
-		self.closecallback(self.name, self.mode)
+		self.closecallback(self.name)
+
+class Constrainer(object):
+	'''
+	Maintains a dict of sizes and a deque to track order of access.
+	Keeps total size under the limit by removing the oldest items.
+	'''
+	debug = False
+	def __init__(self, maxsize, remove_callback):
+		self.max_size = maxsize
+		self.remove_callback = remove_callback
+
+		self.total_size = 0
+		self.size_dict = {}
+		self.order = collections.deque()
+
+		if self.debug:
+			self.print_contents()
+
+	def print_contents(self):
+		print '=============================================================='
+		print 'ORDER', self.order
+		print 'SIZES', self.size_dict
+		print 'STATUS', '%s of %s' % (self.total_size, self.max_size)
+		print '=============================================================='
+
+	def insert(self, o, newsize):
+		if o in self.size_dict:
+			oldsize = self.size_dict[o]
+			self.order.remove(o)
+		else:
+			oldsize = 0
+
+		self.size_dict[o] = newsize
+		self.order.appendleft(o)
+		self.total_size += (newsize - oldsize)
+
+		self.clean()
+		if self.debug:
+			self.print_contents()
+
+	def clean(self):
+		while self.total_size > self.max_size:
+			self.remove_oldest()
+
+	def remove_oldest(self):
+		o = self.order.pop()
+		oldsize = self.size_dict[o]
+		del self.size_dict[o]
+		self.total_size -= oldsize
+		self.remove_callback(o)
 
 # Compare two tuples based on their second value.
 # This is used for sorting list of (filename, time) by time.
 def cmp2(a, b):
 	return cmp(a[1], b[1])
+
 
 class CacheFS(fs.osfs.OSFS):
 	'''
@@ -49,52 +99,20 @@ class CacheFS(fs.osfs.OSFS):
 	'''
 	def __init__(self, cachedir, maxsize):
 		fs.osfs.OSFS.__init__(self, cachedir)
-		self.max_size = maxsize
+		self.constrainer = Constrainer(maxsize, self.remove)
 
 		files = list(self.walkfiles())
 
-		filesizes = [self.getsize(f) for f in files]
-		self.size_dict = dict(itertools.izip(files,filesizes))
-
 		fileatimes = [(f,self.getinfo(f)['accessed_time']) for f in files]
 		fileatimes.sort(cmp2)
-		self.order = collections.deque()
-		self.order.extendleft([fa[0] for fa in fileatimes])
+		files = [fa[0] for fa in fileatimes]
 
-		self.total_size = sum(filesizes)
+		for f in files:
+			self.constrainer.insert(f, self.getsize(f))
 
-		self.clean()
-
-	def close_callback(self, name, mode):
-		# update size
-		if mode != 'r':
-			try:
-				oldsize = self.size_dict[name]
-				existing = True
-			except KeyError:
-				existing = False
-				oldsize = 0
-			newsize = self.getsize(name)
-			self.total_size += (newsize - oldsize)
-			self.size_dict[name] = newsize
-			self.clean()
-		else:
-			existing = True
-		# update order
-		if existing:
-			# In an attempt to fix Bug #1762, catch the exception, but still
-			# not sure why it happens.
-			try:
-				# The remove method of a deque can be slow for items
-				# farther right in the deque
-				self.order.remove(name)
-			except ValueError:
-				debug('NEED TO FIX:  self.order does not contain %s' % (name,))
-		self.order.appendleft(name)
-
-	def clean(self):
-		while self.total_size > self.max_size:
-			self.remove_oldest()
+	def close_callback(self, name):
+		newsize = self.getsize(name)
+		self.constrainer.insert(name, newsize)
 
 	def open(self, *args, **kwargs):
 		'''
@@ -103,23 +121,20 @@ class CacheFS(fs.osfs.OSFS):
 		'''
 		f = fs.osfs.OSFS.open(self, *args, **kwargs)
 		name = args[0]
-		f = FileObjectWrapper(f, self.close_callback, name, f.mode)
+		f = FileObjectWrapper(f, self.close_callback, name)
 		return f
 
-	def remove_oldest(self):
-		oldest = self.order.pop()
-		self.remove(oldest)
+	def remove(self, *args, **kwargs):
+		'''
+		Override default remove to also remove parent directory if it
+		is empty.
+		'''
+		fs.osfs.OSFS.remove(self, *args, **kwargs)
+		name = args[0]
 		try:
-			self.removedir(os.path.dirname(oldest), recursive=True)
+			self.removedir(os.path.dirname(name), recursive=True)
 		except fs.errors.DirectoryNotEmptyError:
 			pass
-
-	def remove(self, *args, **kwargs):
-		name = args[0]
-		oldsize = self.size_dict[name]
-		del self.size_dict[name]
-		self.total_size -= oldsize
-		return fs.osfs.OSFS.remove(self, *args, **kwargs)
 
 test_cache_dir = 'cachedir'
 
